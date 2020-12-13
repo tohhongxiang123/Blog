804fac58d8eb56dd4207384062bd0d297b7c2f8d
tohhongxiang123 <tohhongxiang@gmail.com>
2020-12-13 20:10:42 +0800

    chore: Remove github workflow

--- next.config.js
+++ next.config.js
@@ -5,14 +5,5 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
-    },
-    exportPathMap: async function(
-        defaultPathMap, { dev, dir, outDir, distDir, buildId }
-    ) {
-        return {
-            '/': { page: '/' },
-            '../posts': { page: '/posts' },
-            '../notes': { page: '/notes' }
-        }
     }
 }
--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -39,6 +39,6 @@ export const getStaticPaths = async () => {
 
 	return {
 		paths,
-		fallback: false,
+		fallback: true,
 	}
 }
