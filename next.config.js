784c790b919e0692f85220623fd4b01b6abdd54e
tohhongxiang123 <tohhongxiang@gmail.com>
2020-12-13 18:31:28 +0800

    feat: add github actions to automatically deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const path = require('path')
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
+    assetPrefix: isProd ? 'https://cdn.statically.io/gh/tohhongxiang123/tohhongxiang123.github.io/gh-pages/' : '',
 }
